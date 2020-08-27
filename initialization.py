ff5e267a06bd8bfcf0c07dd9ec7210e0cbbc1cb7
hosseinxj0152 <hosseinxj0152hayabusa@gmail.com>
2020-08-27 13:39:42 +0430

    This is commit 002

--- initialization.py
+++ initialization.py
@@ -1,18 +1,37 @@
 from classes import *
 
+
 class AirPlaneFactory(Factory):
-  def __init__(self, hp, base_cost, zone):
-    super().__init__(hp, base_cost)
+    def __init__(self, zone):
+        super().__init__(100, {"Fuel": 100, "Money": 100, "Iron": 100})
+        self.zone = zone
+
+
 class AntiAircraftFactory(Factory):
-  def __init__(self, hp, base_cost, zone):
-    super().__init__(hp, base_cost)
+    def __init__(self, zone):
+        super().__init__(100, {"Fuel": 100, "Money": 100, "Iron": 100})
+        self.zone = zone
+
+
 class ArmamentFactory(Factory):
-  def __init__(self, hp, base_cost, zone):
-    super().__init__(hp, base_cost)
+    def __init__(self, zone):
+        super().__init__(100, {"Fuel": 100, "Money": 100, "Iron": 100})
+        self.zone = zone
+
+
+forest_zone = Zone(0.5, [1, 1, 1])
+plain_zone = Zone(0.7, [2, 1, 3])
+mountain_zone = Zone(0.1, [3, 5, 4])
 
 
 def main():
-    print("Hello World!")
+    starting_resources = {"Fuel": 1000, "Money": 1000, "Iron": 1000}
+    p1 = Player("810977144", 0, 0, starting_resources)
+    temp_factory = AirPlaneFactory(forest_zone)
+    p1.addFactory(temp_factory)
+    temp_factory = AirPlaneFactory(mountain_zone)
+    p1.addFactory(temp_factory)
+    print(p1.factories[0].zone)
 
 if __name__ == "__main__":
     main()
--- miscellaneousFunctions.py
+++ miscellaneousFunctions.py
@@ -1,16 +1,17 @@
 def subtractLists(first_list, second_list):
-  if(len(first_list)!=len(second_list)):
-    print("The lengths of your lists are not the same! Please try again!")
-  else:
-    return [x - y for (x, y) in (first_list, second_list)]
+    result = first_list.copy()
+    result={int(x)-int(y) for (x,y) in zip(first_list,second_list)}
+    return result
+    # return {[*first_list]:(x-y) for (x, y) in zip(first_list, second_list)}
+
+
 def multiplyLists(first_list, second_list):
-  if(len(first_list)!=len(second_list)):
-    print("The lengths of your lists are not the same! Please try again!")
-  else:
-    return [x * y for (x, y) in (first_list, second_list)]
+    return {x * y for (x, y) in zip(first_list, second_list)}
+
 
 def main():
     print("Hello World!")
+
 
 if __name__ == "__main__":
     main()
