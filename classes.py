44cb585c5c78e3fea444068a96ae6ea9b518940b
hosseinxj0152 <hosseinxj0152hayabusa@gmail.com>
2020-12-09 00:06:45 +0330

    updated after 8-9/12/2020 Meeting!

--- classes.py
+++ classes.py
@@ -19,10 +19,31 @@ class Zone:
 
 
 class Product:
-    def __init__(self, name, materials, production_time):
+    def __init__(self, name, materials, production_time, factory_type):
         self.name = name
         self.materials = materials
         self.production_time = production_time
+        self.factory_type = factory_type
+
+
+class aircraft(Product):
+    def __init__(self, name, materials, production_time, caliber=None, capacity=None):
+        super().__init__(name, materials, production_time,"AirCraftFactory")
+        self.caliber = caliber
+        self.capacity = capacity
+
+
+class antiaircraft(Product):
+    def __init__(self, name, materials, production_time, caliber=None, capacity=None):
+        super().__init__(name, materials, production_time,"AntiAirCraftFactory")
+        self.caliber = caliber
+        self.capacity = capacity
+
+
+class armament(Product):
+    def __init__(self, name, materials, production_time, damage):
+        super().__init__(name, materials, production_time,"ArmamentFactory")
+        self.damage = damage
 
 
 class Player:
@@ -32,29 +53,57 @@ class Player:
         self.posy = posy
         self.resources = starting_resources
         self.is_working = False
-        self.factories = {"AirCraftFactory":0,
-                          "AntiAircraftFactory":0,
-                          "ArmamentFactory":0
-        }
-        self.inventory = {"UAV": 0,
-                          "Bomber": 0}
+        self.is_building = False
+        self.factories = {"AirCraftFactory": 0,
+                          "AntiAircraftFactory": 0,
+                          "ArmamentFactory": 0
+                          }
+        self.inventory = {"Reconnaissance_aircraft": 0,
+                          "B_2_Spirit": 0,
+                          "Caliber_35":0,
+                          "Caliber_20":0,
+                          "Marksman":0,
+                          "Macbeth":0}
 
     def buildFactory(self, input_factory):
-        self.is_working = True
-        self.factories[input_factory.name]+=1
+        self.is_building = True
+        self.factories[input_factory.name] += 1
         input_factory_cost = multiplyDicts(input_factory.base_cost, input_factory.zone.cost_ratio)
         self.resources = subtractDicts(self.resources, input_factory_cost)
-        self.is_working = False
+        self.is_building = False
 
     def addToInventory(self, product):
         self.inventory[product.name] += 1
 
     def produce(self, factory, product):
-        self.is_working = True
-        self.resources = subtractDicts(self.resources, product.materials)
-        time.sleep(product.production_time)
-        self.addToInventory(product)
-        self.is_working = False
+        if(factory.name == product.factory_type):
+            self.is_working = True
+            self.resources = subtractDicts(self.resources, product.materials)
+            time.sleep(product.production_time)
+            self.addToInventory(product)
+            self.is_working = False
+
+
+class AirCraftFactory(Factory):
+    def __init__(self, zone):
+        super().__init__(100, {"Fuel": 100, "Money": 100, "Iron": 100}, "AirCraftFactory")
+        self.zone = zone
+        self.name = "AirCraftFactory"
+
+
+class AntiAircraftFactory(Factory):
+    def __init__(self, zone):
+        super().__init__(100, {"Fuel": 100, "Money": 100, "Iron": 100}, "AntiAirCraftFactory")
+        self.zone = zone
+        self.name = "AntiAirCraftFactory"
+
+
+class ArmamentFactory(Factory):
+    def __init__(self, zone):
+        super().__init__(100, {"Fuel": 100, "Money": 100, "Iron": 100}, "ArmamentFactory")
+        self.zone = zone
+        self.name = "ArmamentFactory"
+
 
 def main():
     print("Hello World!")
--- initialization.py
+++ initialization.py
@@ -1,30 +1,30 @@
 from classes import *
 
-
-class AirCraftFactory(Factory):
-    def __init__(self, zone):
-        super().__init__(100, {"Fuel": 100, "Money": 100, "Iron": 100}, "AirCraftFactory")
-        self.zone = zone
-        self.name = "AirCraftFactory"
-
-    UAV = Product("UAV", {"Fuel": 10, "Money": 10, "Iron": 10}, 3)
-    Bomber = Product("Bomber", {"Fuel": 15, "Money": 15, "Iron": 15}, 5)
-
-
-class AntiAircraftFactory(Factory):
-    def __init__(self, zone):
-        super().__init__(100, {"Fuel": 100, "Money": 100, "Iron": 100}, "AntiAirCraftFactory")
-        self.zone = zone
-        self.name = "AntiAirCraftFactory"
-
-
-class ArmamentFactory(Factory):
-    def __init__(self, zone):
-        super().__init__(100, {"Fuel": 100, "Money": 100, "Iron": 100}, "ArmamentFactory")
-        self.zone = zone
-        self.name = "ArmamentFactory"
-
-
+Caliber_35 = armament(name="Caliber_35",
+                      materials={"Fuel": 0.002, "Money": 0.002, "Iron": 0.002},
+                      production_time=3,
+                      damage=0.1)
+Caliber_20 = armament(name="Caliber_20",
+                      materials={"Fuel": 0.002, "Money": 0.002, "Iron": 0.002},
+                      production_time=0.00001,
+                      damage=.07)
+Reconnaissance_aircraft = aircraft(name="Reconnaissance_aircraft",
+                                   materials={"Fuel": 10, "Money": 10, "Iron": 10},
+                                   production_time=3)
+B_2_Spirit = aircraft(name="B_2_Spirit",
+                      materials={"Fuel": 15, "Money": 15, "Iron": 15},
+                      production_time=4,
+                      caliber=Caliber_35, capacity=5)
+Marksman = antiaircraft(name="Marksman",
+                        materials={"Fuel": 5, "Money": 5, "Iron": 5},
+                        production_time=2,
+                        caliber=Caliber_35,
+                        capacity=10)
+Macbeth = antiaircraft(name="Macbeth",
+                       materials={"Fuel": 4, "Money": 4, "Iron": 4},
+                       production_time=1,
+                       caliber=Caliber_20,
+                       capacity=15)
 forest_zone = Zone(0.5, {"Fuel": 1, "Money": 1, "Iron": 1}, "forest_zone")
 plain_zone = Zone(0.7, {"Fuel": 1, "Money": 1, "Iron": 1}, "plain_zone")
 mountain_zone = Zone(0.1, {"Fuel": 1, "Money": 1, "Iron": 1}, "mountain_zone")
@@ -33,9 +33,9 @@ mountain_zone = Zone(0.1, {"Fuel": 1, "Money": 1, "Iron": 1}, "mountain_zone")
 def main():
     starting_resources = {"Fuel": 1000, "Money": 1000, "Iron": 1000}
     p1 = Player("810977144", 0, 0, starting_resources)
-    temp_factory = AirCraftFactory(plain_zone)
+    temp_factory = ArmamentFactory(plain_zone)
     p1.buildFactory(temp_factory)
-    p1.produce(temp_factory, temp_factory.UAV)
+    p1.produce(temp_factory, Caliber_35)
     print(p1.inventory)
     print(p1.resources)
     print(p1.factories)
